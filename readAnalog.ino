58dc19e4c496673a5c8d510a6ba7fff66b86160d
stn04 <133672174+stn04@users.noreply.github.com>
2023-06-12 19:27:58 +0200

    Create readAnalog.ino

---
+++ readAnalog.ino
@@ -0,0 +1,26 @@
+int analogValue = 0;    // variable to hold the analog value
+
+void setup() {
+  // open the serial port at 9600 bps:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(13, LOW);
+  // read the analog input on pin 0:
+  analogValue = analogRead(0);
+
+  // print it out in many formats:
+  Serial.println(analogValue);       // print as an ASCII-encoded decimal
+  Serial.println(analogValue, DEC);  // print as an ASCII-encoded decimal
+  Serial.println(analogValue, HEX);  // print as an ASCII-encoded hexadecimal
+  Serial.println(analogValue, OCT);  // print as an ASCII-encoded octal
+  Serial.println(analogValue, BIN);  // print as an ASCII-encoded binary
+
+  // delay 10 milliseconds before the next reading:
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(100);
+}
